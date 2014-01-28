8121db73cdf831be0d5ba4e5e6351ec5c965ee15
Fabrizio Furnari <fab.furnari@gmail.com>
2014-01-28 15:03:45 +0100

    more logging functions

--- build_package.sh
+++ build_package.sh
@@ -7,24 +7,32 @@ PACKAGE_NAME=$(grep Source ./debian/control | awk '{print $2}')
 LOGFILE=$HOME/log/$PACKAGE_NAME-build.log
 BUILD_TYPE=$1
 
-logit () { echo "$(date): $@" >> $LOGFILE; }
+log() 
+{
+  while read data
+  do
+      echo "[ $(date +"%D %T")] $data" >> $LOGFILE
+  done
+}
 
-logit "START"
+say () { echo "$(date): $@" >> $LOGFILE; }
+
+say "START"
 if [[ "$BUILD_TYPE" == "stable" ]]; then
-    logit "Setting incoming dir for stable releases"
+    say "Setting incoming dir for stable releases"
     INCOMING_DIR=/srv/incoming
 elif [[ "$BUILD_TYPE" == "dev" ]]; then
-    logit "Setting incoming dir for development releases"
+    say "Setting incoming dir for development releases"
     INCOMING_DIR=/srv/devincoming
 else
-    logit "Unrecognizable argument"
+    say "Unrecognizable argument"
     exit 2
 fi
 
-logit "Starting build..." 
+say "Starting build..." 
 
 if [ -z "$PACKAGE_NAME" ]; then
-    logit "Cannot match package name, exiting ..." 
+    say "Cannot match package name, exiting ..." 
     exit 2
 fi
 
@@ -32,33 +40,32 @@ WORKDIR=$EXPORT_DIR/$PACKAGE_NAME
 
 if [ -d $WORKDIR ];
 then
-    logit "$WORKDIR existing, removing..." 
+    say "$WORKDIR existing, removing..." 
     rm -fr $WORKDIR
     mkdir $WORKDIR
 fi
 
-logit "Starting building amd64 package..." 
-/usr/bin/git-buildpackage --git-arch=amd64 --git-pbuilder --git-dist=$GIT_DIST --git-export-dir=$WORKDIR 
+say "Starting building amd64 package..." 
+/usr/bin/git-buildpackage --git-arch=amd64 --git-pbuilder --git-dist=$GIT_DIST --git-export-dir=$WORKDIR | log
 
-logit "Starting building i386 package" 
-/usr/bin/git-buildpackage --git-arch=i386 --git-pbuilder --git-dist=$GIT_DIST --git-export-dir=$WORKDIR 
+say "Starting building i386 package" 
+/usr/bin/git-buildpackage --git-arch=i386 --git-pbuilder --git-dist=$GIT_DIST --git-export-dir=$WORKDIR | log
 
 cd $WORKDIR
 CHANGES64=$(ls *amd64*changes)
 CHANGES32=$(ls *i386*changes)
 
-logit "Merging changes..." 
+say "Merging changes..." 
 mergechanges -f $CHANGES32 $CHANGES64
 MULTI_CHANGES=$(ls *multi*changes)
-logit "Created merged changes file: $MULTI_CHANGES" 
+say "Created merged changes file: $MULTI_CHANGES" 
 
-logit "Running lintian on $MULTI_CHANGES..." 
-lintian $MULTI_CHANGES 
+say "Running lintian on $MULTI_CHANGES..." 
+lintian $MULTI_CHANGES | log
 
-logit "Copying files to $INCOMING_DIR..." 
-cp $(cat $MULTI_CHANGES | awk '/Files:/,0' | tail -n +2 | awk '{print $5}') $INCOMING_DIR
-cp $MULTI_CHANGES $INCOMING_DIR
+say "Copying files to $INCOMING_DIR..." 
+cp -v $(cat $MULTI_CHANGES | awk '/Files:/,0' | tail -n +2 | awk '{print $5}') $INCOMING_DIR | log
+cp -v $MULTI_CHANGES $INCOMING_DIR | log
 
-logit "END" 
+say "END" 
 echo
- 
