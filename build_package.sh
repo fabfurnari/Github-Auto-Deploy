0624d3d1fc48306bc36055400fba1a2fe57f414e
Fabrizio Furnari <fab.furnari@gmail.com>
2014-01-28 14:51:26 +0100

    starting to migrate to logit func

--- build_package.sh
+++ build_package.sh
@@ -5,11 +5,17 @@ EXPORT_DIR=$HOME/build-area/
 OTHER_GIT_OPTIONS=""
 PACKAGE_NAME=$(grep Source ./debian/control | awk '{print $2}')
 LOGFILE=$HOME/log/$PACKAGE_NAME-build.log
+BUILD_TYPE=$1
 
-if [ $1 == 'stable' ]; then
+function logit(){
+    echo "$(date): $@" >> $LOGFILE
+}
+
+logit "START"
+if [[ "$BUILD_TYPE" == "stable" ]]; then
     echo "Setting incoming dir for stable releases"
     INCOMING_DIR=/srv/incoming
-elif [ $1 == 'dev' ]; then
+elif [[ "$BUILD_TYPE" == "dev" ]]; then
     echo "Setting incoming dir for development releases"
     INCOMING_DIR=/srv/devincoming
 else
@@ -35,9 +41,11 @@ then
 fi
 
 echo "********* $(date) ***********" | tee -a $LOGFILE
+echo "Starting building amd64 package..." | tee -a $LOGFILE
 /usr/bin/git-buildpackage --git-arch=amd64 --git-pbuilder --git-dist=$GIT_DIST --git-export-dir=$WORKDIR | tee -a $LOGFILE
 
 echo "********* $(date) ***********" | tee -a $LOGFILE
+echo "Starting building i386 package" | tee -a $LOGFILE
 /usr/bin/git-buildpackage --git-arch=i386 --git-pbuilder --git-dist=$GIT_DIST --git-export-dir=$WORKDIR | tee -a $LOGFILE
 
 cd $WORKDIR
@@ -45,19 +53,22 @@ CHANGES64=$(ls *amd64*changes)
 CHANGES32=$(ls *i386*changes)
 
 echo "********* $(date) ***********" | tee -a $LOGFILE
+echo "Merging changes..." | tee -a $LOGFILE
 mergechanges -f $CHANGES32 $CHANGES64
 MULTI_CHANGES=$(ls *multi*changes)
 echo "Created merged changes file: $MULTI_CHANGES" | tee -a $LOGFILE
 
 echo "********* $(date) ***********" | tee -a $LOGFILE
+echo "Running lintian on $MULTI_CHANGES..." | tee -a $LOGFILE
 lintian $MULTI_CHANGES | tee -a $LOGFILE
 
 echo "********* $(date) ***********" | tee -a $LOGFILE
-echo "Copying files to $INCOMING_DIR" | tee -a $LOGFILE
+echo "Copying files to $INCOMING_DIR..." | tee -a $LOGFILE
 cp $(cat $MULTI_CHANGES | awk '/Files:/,0' | tail -n +2 | awk '{print $5}') $INCOMING_DIR
 cp $MULTI_CHANGES $INCOMING_DIR
 
 echo "********* $(date) ***********" | tee -a $LOGFILE
+echo "END" | tee -a $LOGFILE
 echo
 echo 
 
